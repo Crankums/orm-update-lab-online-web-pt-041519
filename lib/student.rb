3b9f4bb66235d9de3f141310b5d7e4a803555fae
Craig Donavin <crdonavin@gmail.com>
2019-07-02 14:56:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,7 @@ attr_accessor :id, :name, :grade
   def self.create
     student = Student.new(name, grade)
     student.save
+    student
   end
 
   # Remember, you can access your database connection anywhere in this class
